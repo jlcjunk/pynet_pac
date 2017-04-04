d12819bff9a02ea62b486022de37bbd4557f62c5
jlcjunk <jlcjunk@crabtreefamily.us>
2017-04-04 09:21:07 -0700

    week01 task06 with test files

--- class1/exer06.py
+++ class1/exer06.py
@@ -13,17 +13,19 @@ list = ["text1",3.14159,{"router.ip":"192.168.1.1","router.model":"Cisco2811","s
 
 
 # write YAML file
-yaml_file = open('list.yaml', 'w')
-yaml_file.write("---\n")
-yaml_file.write(yaml.dump(list,default_flow_style=False))
-yaml_file.close()
+with open('list.yaml', 'w') as yaml_file:
+    yaml_file.write("---\n")
+    yaml_file.write(yaml.dump(list,default_flow_style=False))
 
 
 
 
 # write JSON file
-json_file = open('list.json', 'w')
+with open('list.json', 'w') as json_file:
+    json.dump(list,json_file)
 
+
+# note program is done due to lack of other output
 print "Complete"
 
 
