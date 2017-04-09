56673630f98bda666354d4955f2c4274b867da46
jlcjunk <jlcjunk@crabtreefamily.us>
2017-04-09 11:09:18 -0700

    week2 step 2

--- class2/exer1.py
+++ class2/exer1.py
@@ -6,8 +6,10 @@ import paramiko
 import sys
 
 # Part B answer - not sure it's needed
+print "\n\n"
 print "pysnmp version = " + pysnmp.__version__
 print "paramiko version = " + paramiko.__version__
+print "\n"
 
 # Part C answer
 import hello
---
+++ class2/exer2.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+# imports
+import pysnmp
+import paramiko
+import sys
+import telnetlib
+import time
+
+# varsDEVICE_IP
+CON_TIMEOUT = 10
+DEVICE_NAME = 'pynet-rtr1'
+DEVICE_IP = '184.105.247.70'
+DEVICE_PORT = 23
+DEVICE_USER_PROMPT = 'Username'
+DEVICE_USER = 'pyclass'
+DEVICE_PASSWORD_PROMPT = 'Password'
+DEVICE_PASSWORD = '88newclass'
+COMMAND_PROMPT = '#'
+COMMAND = 'show ip int brief'
+
+# connect
+device_connection = telnetlib.Telnet(DEVICE_IP,DEVICE_PORT,CON_TIMEOUT)
+device_connection.read_until(DEVICE_USER_PROMPT)
+time.sleep(1)
+device_connection.write(DEVICE_USER)
+device_output = device_connection.read_very_eager()
+device_connection.read_until(DEVICE_PASSWORD_PROMPT)
+time.sleep(1)
+device_connection.write(DEVICE_PASSWORD)
+device_output = device_connection.unti(COMMAND_PROMPT)
+
+# execute command
+device_output = device_connection.read_very_eager()
+device_connection.write(COMMAND)
+device_output = device_connection.unti(COMMAND_PROMPT)
+
+# close connection
+device_connection.close()
+
+# output results of command
+print device_output
+
