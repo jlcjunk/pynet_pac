971903d7c4a475549901d56dececf9baae88a616
jlcjunk <jlcjunk@crabtreefamily.us>
2017-04-15 13:19:09 -0700

    week2 exer3 test4

--- class2/exer3.py
+++ class2/exer3.py
@@ -13,40 +13,47 @@ import getpass
 TELNET_PORT = 23
 TELNET_TIMEOUT = 6
 
-def send_command(remote_conn, cmd):
-    '''
-    Send a command down the telnet channel
-    Return the response
-    '''
-    cmd = cmd.rstrip()
-    remote_conn.write(cmd + '\n')
-    time.sleep(1)
-    return remote_conn.read_very_eager()
+class MyTelnet(telnetlib.Telnet):
+'''
+super set of Telnet class
 
-def login(remote_conn, username, password):
-    '''
-    Login to network device
-    '''
-    output = remote_conn.read_until("sername:", TELNET_TIMEOUT)
-    remote_conn.write(username + '\n')
-    output += remote_conn.read_until("ssword:", TELNET_TIMEOUT)
-    remote_conn.write(password + '\n')
-    return output
+__init__(self, host=None, port=0, timeout=<object object>)
+'''
 
-def disable_paging(remote_conn, paging_cmd='terminal length 0'):
-    '''
-    Disable the paging of output (i.e. --More--)
-    '''
-    return send_command(remote_conn, paging_cmd)
+    def send_command(self, cmd):
+        '''
+        Send a command down the telnet channel
+        Return the response
+        '''
+        cmd = cmd.rstrip()
+        remote_conn.write(cmd + '\n')
+        time.sleep(1)
+        return remote_conn.read_very_eager()
 
-def telnet_connect(ip_addr):
-    '''
-    Establish telnet connection
-    '''
-    try:
-        return telnetlib.Telnet(ip_addr, TELNET_PORT, TELNET_TIMEOUT)
-    except socket.timeout:
-        sys.exit("Connection timed-out")
+    def login(self, username, password):
+        '''
+        Login to network device
+        '''
+        output = remote_conn.read_until("sername:", TELNET_TIMEOUT)
+        remote_conn.write(username + '\n')
+        output += remote_conn.read_until("ssword:", TELNET_TIMEOUT)
+        remote_conn.write(password + '\n')
+        return output
+
+    def disable_paging(self, paging_cmd='terminal length 0'):
+        '''
+        Disable the paging of output (i.e. --More--)
+        '''
+        return send_command(remote_conn, paging_cmd)
+
+    def telnet_connect(self,ip_addr):
+        '''
+        Establish telnet connection
+        '''
+        try:
+            return telnetlib.Telnet(ip_addr, TELNET_PORT, TELNET_TIMEOUT)
+        except socket.timeout:
+            sys.exit("Connection timed-out")
 
 
 def main():
@@ -57,16 +64,16 @@ def main():
     ip_addr = raw_input("IP address: ")
     ip_addr = ip_addr.strip()
     username = 'pyclass'
-    password = getpass.getpass()
+    password = '88
 
-    remote_conn = telnet_connect(ip_addr)
-    output = login(remote_conn, username, password)
+    remote_conn = MyTelnet.connect(ip_addr)
+    remote_conn.login(username, password)
 
-    time.sleep(1)
-    remote_conn.read_very_eager()
-    disable_paging(remote_conn)
+    #time.sleep(1)
+    #remote_conn.read_very_eager()
+    #disable_paging(remote_conn)
 
-    output = send_command(remote_conn, 'show ip int brief')
+    #output = send_command(remote_conn, 'show ip int brief')
 
     print "\n\n"
     print output
