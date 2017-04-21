0431a69529b331a6aead9e49ee10eed5b6f4e460
jlcjunk <jlcjunk@crabtreefamily.us>
2017-04-20 21:48:57 -0700

    week3 task 1 done

--- class3/exer01.py
+++ class3/exer01.py
@@ -1,17 +1,14 @@
 #!/usr/bin/env python
 
 '''
-create first data file
+check last changed config time and send notice if config has changed since last check
 '''
 
 # imports
 try:
-    import sys
-    import pysnmp
     import yaml
-    import telnetlib
-    import time
     import snmp_helper
+    import email_helper
 except ImportError:
     print "Could not import a required module.\n Exiting"
     raise SystemExit
@@ -21,32 +18,57 @@ except ImportError:
 DATA_FILE = 'dev_list'
 
 
-# create seed data structure
-device_list = {
-    'pynet-rtr2':['184.105.247.71',161,'pysnmp','SHA','galileo1','AES','galileo1','1.3.6.1.4.1.9.9.43.1.1.1.0','0'],
-    'pynet-rtr1':['184.105.247.70',161,'pysnmp','SHA','galileo1','AES','galileo1','1.3.6.1.4.1.9.9.43.1.1.1.0','0']
-    }
+def main():
+    '''
+    simple test with SNMP to see if config has changed and send notice if it has.
+    '''
+    # read data file - devices to check and last modified time
+    try:
+        with open(DATA_FILE + '.yaml', 'r') as yaml_file:
+            device_list = yaml.load(yaml_file)
+    except yaml.scanner.ScannerError:
+        print "Data file is corrupt."
+        raise SystemExit
 
-# print device list
-print '\n'
-print device_list
-print '\n'
+    for listed_device in device_list:
+        # save last timestamp for later use
+        last_known_change = device_list[listed_device][8]
+
+        # Prepare to get timestamp from device
+        snmp_device = (device_list[listed_device][0], device_list[listed_device][1])
+        snmp_user = (device_list[listed_device][2], device_list[listed_device][4], device_list[listed_device][6])
+        oid = device_list[listed_device][7]
+        auth_protocol = device_list[listed_device][3]
+        encrypt_protocol = device_list[listed_device][5]
+
+        # get timestamp of change from device
+        try:
+            last_changed = snmp_helper.snmp_extract(snmp_helper.snmp_get_oid_v3(snmp_device, snmp_user, oid=oid, auth_proto=auth_protocol, encrypt_proto=encrypt_protocol, display_errors=False))
+        except TypeError:
+            print 'There was an error connecting to ' + listed_device + ' via SNMP. \n Device skipped.'
+            last_changed = last_known_change
+
+        # provide some visual output for the sake of knowing something was done when timestamps match
+        print listed_device
+
+        # act if timestamps do not match
+        if last_known_change != last_changed:
+            print 'Configuration does not match. Sending email.'
+            email_helper.send_mail('jlcjunk@crabtreefamily.us', 'Configuration channge on ' + listed_device, 'Configuration channge on ' + listed_device, 'pythonlab@crabtreefamily.us')
+
+        # allow visual timestamp comparison to verify code is working
+        print '    ' + last_known_change
+        print '    ' + last_changed
+
+        # Save new timestamp so it can be saved later for subsiquent runs
+        device_list[listed_device][8] = last_changed
 
 
-# create data file
-with open(DATA_FILE + '.yaml', 'w') as yaml_file:
-    yaml_file.write(yaml.dump(device_list,default_flow_style=None,width=120))
-
-
-# read YAML file
-with open(DATA_FILE + '.yaml','r') as yaml_file:
-    device_list = yaml.load(yaml_file)
+    # save new timestamps
+    with open(DATA_FILE + '.yaml', 'w') as yaml_file:
+        yaml_file.write(yaml.dump(device_list, default_flow_style=None, width=200))
 
 
 
-# print reconstituted data so I know it ended w/o dieing and data reads ok
-print '\n' 
-print device_list
-print '\n'
-
-
+if __name__ == "__main__":
+    main()
